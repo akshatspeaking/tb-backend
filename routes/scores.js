dde7ec714fa6b60fef0d94865f2e4942a36cdb68
akshatspeaking <akshatspeaking@gmail.com>
2021-09-07 13:49:08 +0530

    update getscores response

--- routes/scores.js
+++ routes/scores.js
@@ -4,11 +4,18 @@ const Score = require("../models/Score");
 
 router.get("/", async (req, res) => {
   try {
-    // console.log("Get scoreboard");
-    const scoreList = await Score.find()
-      .limit(10)
-      .sort({ score: -1 })
-      .res.json(scoreList);
+    const scoreList = await Score.find().sort({ score: -1 });
+
+    const numberOfGames = scoreList.length;
+
+    const averageScore =
+      scoreList.reduce((acc, cv) => {
+        return acc + cv.score;
+      }, 0) / numberOfGames;
+
+    const topten = scoreList.slice(0, 10);
+
+    res.status(200).json({ scoreList: topten, numberOfGames, averageScore });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -34,12 +41,12 @@ router.post("/", async (req, res) => {
         return acc + cv.score;
       }, 0) / numberOfGames;
 
-    const stats = { numberOfGames, averageScore };
-
     const topten = scoreList.slice(0, 10);
 
     //
-    res.status(200).json({ scoreList: topten, rank, stats });
+    res
+      .status(200)
+      .json({ scoreList: topten, rank, numberOfGames, averageScore });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
