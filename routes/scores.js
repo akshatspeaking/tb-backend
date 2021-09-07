3e4e2d2d2d80a0982475560be4109132804b1afb
akshatspeaking <akshatspeaking@gmail.com>
2021-09-07 10:55:55 +0530

    correct sort

--- routes/scores.js
+++ routes/scores.js
@@ -5,8 +5,10 @@ const Score = require("../models/Score");
 router.get("/", async (req, res) => {
   try {
     // console.log("Get scoreboard");
-    const scoreList = await Score.find().sort({ score: -1 }).limit(10);
-    res.json(scoreList);
+    const scoreList = await Score.find()
+      .limit(10)
+      .sort({ score: -1 })
+      .res.json(scoreList);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -25,10 +27,19 @@ router.post("/", async (req, res) => {
       scoreList.find((obj) => obj.id === newScore.id)
     );
 
+    const numberOfGames = scoreList.length;
+
+    const averageScore =
+      scoreList.reduce((acc, cv) => {
+        return acc + cv.score;
+      }, 0) / numberOfGames;
+
+    const stats = { numberOfGames, averageScore };
+
     const topten = scoreList.slice(0, 10);
 
     //
-    res.status(200).json({ scoreList: topten, rank });
+    res.status(200).json({ scoreList: topten, rank, stats });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
